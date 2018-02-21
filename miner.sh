d8e00e293e13aa6a173bf24114f17399bfbab793
Mike Reeves <mike.reeves@securityonionsolutions.com>
2018-02-20 20:29:46 -0500

    Update Unraid Template

--- miner.sh
+++ miner.sh
@@ -2,6 +2,10 @@
 
 cp /etc/creeper/mining.conf $CREEP_MINER_DATADIR/mining.conf
 
+echo "Replacing some stuff"
+echo $MADDRESS
+echo $PORT
+
 sed -i -e 's/MININGPOOLADDRESS/$MADDRESS/g' $CREEP_MINER_DATADIR/mining.conf
 sed -i -e 's/MPORT/$PORT/g' $CREEP_MINER_DATADIR/mining.conf
 
