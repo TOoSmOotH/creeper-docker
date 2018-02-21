2ca05b9cea578c78cce435915126c8ac3f9cc59d
Mike Reeves <mike.reeves@securityonionsolutions.com>
2018-02-20 21:05:52 -0500

    Change the version

--- miner.sh
+++ miner.sh
@@ -6,7 +6,8 @@ echo "Replacing some stuff"
 echo $MADDRESS
 echo $PORT
 
-sed -i -e 's/MININGPOOLADDRESS/$MADDRESS/g' $CREEP_MINER_DATADIR/mining.conf
-sed -i -e 's/MPORT/$PORT/g' $CREEP_MINER_DATADIR/mining.conf
+sed -i -e "s/MININGPOOLADDRESS/$MADDRESS/g" $CREEP_MINER_DATADIR/mining.conf
+sed -i -e "s/MPORT/$PORT/g" $CREEP_MINER_DATADIR/mining.conf
+cat $CREEP_MINER_DATADIR/mining.conf
 
 ./creepMiner $CREEP_MINER_DATADIR/mining.conf
